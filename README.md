a47b3c44ed8fd86f3b9b27b65aa1303643910241
Sam Morrison <sam@cnstechgroup.com>
2016-08-22 01:56:54 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+Role: cns-codedeploy-agent
+========
+
+This role installs and configures the AWS codedeploy agent for a debian EC2 instance.
+
+Requirements
+------------
+
+Nothing, it runs out of the box.
+
+Role Variables
+--------------
+
+In the current version, you can specify the following variables:
+
+| Name | Default |   |
+|------|---------|---|
+| ---  |   ---   | --- |
+
+Dependencies
+------------
+
+This package has no dependencies.
+
+License
+-------
+
+GPLv2
+
+Author Information
+------------------
+
+* [Sam Morrison](https://www.twitter.com/samcns)
---
+++ files/rvm-installer.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Install mpapis public key (might need `gpg2` and or `sudo`)
+# gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+
+# Download the installer
+\curl -O https://raw.githubusercontent.com/rvm/rvm/master/binscripts/rvm-installer
+\curl -O https://raw.githubusercontent.com/rvm/rvm/master/binscripts/rvm-installer.asc
+
+# Verify the installer signature (might need `gpg2`), and if it validates...
+gpg --verify rvm-installer.asc &&
+
+  # Run the installer
+bash rvm-installer stable --ruby=2.1.1
+
+source ~/.rvm/scripts/rvm
+
+rvm --default use 2.1.1
